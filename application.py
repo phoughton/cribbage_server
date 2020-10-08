ccb9ef9f69614ca7d0c250785ba1947cf9481750
Peter Houghton <pete@investigatingsoftware.co.uk>
2020-10-08 14:39:10 +0100

    Added show and play methods

--- application.py
+++ application.py
@@ -1,9 +1,18 @@
-from flask import Flask, abort, request, jsonify
+from flask import Flask, request, jsonify
 import errors
 
 from cribbage_scorer import cribbage_scorer as cs
 
 application = Flask(__name__)
+
+
+def lists_to_tuples(list_of_lists):
+    out_list = []
+    for a_list in list_of_lists:
+        if len(a_list) > 2:
+            raise Exception("Error: Card ID list can not be converted to tuple as too many members.")
+        out_list.append(tuple(a_list))
+    return out_list
 
 
 @application.errorhandler(errors.InvalidUsage)
@@ -23,18 +32,36 @@ def cutcalcscore():
     cut_card = tuple(req_data["cut_card"])
     dealer = req_data["dealer"]
 
-    calc_scores, msg  = cs.cut_calc_score(cut_card, players, dealer)
+    scores, msg = cs.cut_calc_score(cut_card, players, dealer)
 
-    return {"score": calc_scores, "message": msg}
+    return {"type": "cut", "score": scores, "message": msg}
 
 
-@application.route("/score/play", methods=["GET", "POST"])
+@application.route("/score/play", methods=["POST"])
 def playcalcscore():
-    pass
+
+    req_data = request.get_json()
+    errors.check_for_required_fields(req_data, ["players", "played_cards"])
+
+    players = req_data["players"]
+    played_cards = lists_to_tuples(req_data["played_cards"])
+
+    scores, current_count, play_log = cs.play_calc_score_whole_game(played_cards, players)
+
+    return {"type": "play", "score": scores, "current_count": current_count, "play_log": play_log}
 
 
-@application.route("/score/show", methods=["GET", "POST"])
+@application.route("/score/show", methods=["POST"])
 def showcalcscore():
-    pass
 
+    req_data = request.get_json()
+    errors.check_for_required_fields(req_data, ["starter", "hand", "crib"])
 
+    starter = tuple(req_data["starter"])
+    hand = lists_to_tuples(req_data["hand"])
+    crib = req_data["crib"]
+
+    print(starter, hand, crib)
+    score, msg = cs.show_calc_score(starter, hand, crib)
+
+    return {"type": "show", "score": score, "message": msg}
