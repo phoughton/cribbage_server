5c761aed2daa6c5a0c5ddb707f92ba5d0d2166d9
Pete Houghton <pete@investigatingsoftware.co.uk>
2021-01-17 17:39:17 +0000

    Add error checks for show stage

---
+++ tests/show_test.py
@@ -0,0 +1,27 @@
+import pytest
+from cribbage_scorer import cribbage_scorer
+import requests, json
+from cribbage_server import application as cs
+import os
+import tempfile
+
+
+@pytest.fixture
+def client():
+    cs.application.config['TESTING'] = True
+    return cs.application.test_client()
+
+
+@pytest.mark.parametrize("request_data, correct_status_code", [
+        ({"starter": [3,"C"], "dealer": "Abi"}, 400),
+        ({"starter": ["Abi", "Bob"], "dealer": "Abi"}, 400),
+        ({"players": ["Abi", "Bob"], "crib": [[3,"C"]]},400),
+        ({"starter": -5}, 400),
+        ({}, 400),        
+        ([], 400)
+])
+def test_invalid_data_play_setclient(client, request_data, correct_status_code):
+
+    resp = client.post("/score/show", json=request_data)
+
+    assert resp.status_code == correct_status_code
